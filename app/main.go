ca3e5b705043120187fb301d9c71c579da986593
ChevalierSoft <ibanarashi@outlook.fr>
2022-10-29 04:23:47 +0200

    template ready

--- app/main.go
+++ app/main.go
@@ -2,23 +2,6 @@ package main
 
 import "fmt"
 
-// _ "shorter-url/bdd"
-
-// "github.com/gin-contrib/cors"
-// "github.com/gin-gonic/gin"
-
 func main() {
-
-	fmt.Println("jipeps")
-	// err := createSchema(G_db)
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// router := gin.New()
-	// router.Use(cors.Default())
-
-	// router.GET("/")
-
-	// router.Run("80")
+	fmt.Println("Salut")
 }
