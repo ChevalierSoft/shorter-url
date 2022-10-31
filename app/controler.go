6d0a0c0fdfc3c6789a71c412a147c7c7c8f6db68
ChevalierSoft <ibanarashi@outlook.fr>
2022-10-31 04:23:27 +0100

    cleaned code

--- app/controler.go
+++ app/controler.go
@@ -18,10 +18,12 @@ func (controller *HttpController) getLinks(c *gin.Context) {
 
 func (controller *HttpController) setNewLink(c *gin.Context) {
 	var input Link
+
 	if err := c.ShouldBindJSON(&input); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
+
 	link := Link{Url: input.Url}
 	if link.Url == "" {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Url is required"})
@@ -32,9 +34,11 @@ func (controller *HttpController) setNewLink(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Url is not valid"})
 		return
 	}
+
 	_, err = controller.Database.NewInsert().Model(&link).Exec(c)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	c.JSON(http.StatusOK, gin.H{"data": link})
 }
