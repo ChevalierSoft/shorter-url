17c5d89166b0dd884273d6bcf872825d8e8e0d9d
ChevalierSoft <ibanarashi@outlook.fr>
2022-11-01 23:02:22 +0100

    trying air, to run in watch mode

--- app/setup.sh
+++ app/setup.sh
@@ -2,5 +2,9 @@
 
 cd /go/src/app
 go get .
-go run .
-#./shorter-url
+curl -sSfL https://raw.githubusercontent.com/cosmtrek/air/master/install.sh | sh -s -- -b $(go env GOPATH)/bin
+export PATH=$PATH:$(go env GOPATH)/bin
+export air_wd=/go/src/app
+air
+# go run .
+# ./shorter-url
